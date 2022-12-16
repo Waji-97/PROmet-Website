5e6ca8aceac87f61492d024fbec7878c9ea75320
Waji <wajiwos16@gmail.com>
2022-12-16 17:51:41 +0900

    기능

---
+++ acc/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from acc import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'acc'
+
+urlpatterns = [
+  path('login/', auth_views.LoginView.as_view(template_name='acc/login.html'), name='login'),
+  path('signup/', views.signup, name='signup'),
+  path('logout/', auth_views.LogoutView.as_view(), name='logout')
+]
